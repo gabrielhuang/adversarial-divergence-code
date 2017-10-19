dd80ab9c8c9204de9ecb52904d78eec761ccf65b
Hugo Berard <berard.hugo@gmail.com>
2017-10-19 08:03:20 -0700

    Updating VAE

--- visual-hyperplane/train_vae.py
+++ visual-hyperplane/train_vae.py
@@ -1,10 +1,24 @@
 from hyperplane_dataset import HyperplaneCachedDataset
 from torch.utils.data import DataLoader
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import math
+import numpy as np
 
-def softmax(self, input):
-    output = input._zeros()
-    for i in range():
+def softmax(input):
+    output = Variable(torch.zeros(input.size()))
+    for i in range(input.size()[1]):
+        output[:,i] = F.log_softmax(input[:,i])
+    return output
 
+def nll_loss(x, x_pred):
+    output = Variable(torch.zeros(1))
+    for i in range(x.size()[1]):
+        output += F.nll_loss(x_pred[:,i], x[:,i]).float()
+    return output
 
 batch_size = 128
 n_epochs = 1000
@@ -14,13 +28,15 @@ lr = 3e-4
 
 dataset = HyperplaneCachedDataset(25, range(10), n_x)
 data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-print 'Length: %i'%len(dataset)
 
-from torch.autograd import Variable
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import math
+dataiter = iter(data_loader)
+examples = dataiter.next()[:10]
+data = dataset[:]
+for i in range(10):
+    print data.masked_select(data==i).size()
+exit()
+
+print 'Length: %i'%len(dataset)
 
 class VAE(nn.Module):
     def __init__(self, n_x, n_z):
@@ -28,7 +44,7 @@ class VAE(nn.Module):
         self.n_z = n_z
         self.n_x = n_x
 
-        self.enc1 = nn.Linear(10*self.n_x, 500)
+        self.enc1 = nn.Linear(self.n_x, 500)
         self.enc2 = nn.Linear(500, 500)
         self.enc_mu = nn.Linear(500, self.n_z)
         self.enc_logvar = nn.Linear(500, self.n_z)
@@ -38,7 +54,7 @@ class VAE(nn.Module):
         self.dec = nn.Linear(500, 10*self.n_x)
 
     def encode(self, x):
-        x = F.relu(self.enc1(x.view(-1, 10*n_x)))
+        x = F.relu(self.enc1(x))
         x = F.relu(self.enc2(x))
         z_mu = self.enc_mu(x)
         z_logvar = self.enc_logvar(x)
@@ -51,32 +67,44 @@ class VAE(nn.Module):
     def decode(self, z):
         z = F.relu(self.dec1(z))
         z = F.relu(self.dec2(z))
-        x = F.softmax(self.dec(z.view(-1,10))).view(-1,)
+        x = softmax(self.dec(z).view(-1,self.n_x,10))
         return x
 
     def forward(self, x):
         z_mu, z_logvar = self.encode(x)
         z = self.reparametrize(z_mu, z_logvar)
-        x_mu, x_logvar = self.decode(z)
-        return x_mu, x_logvar, z_mu, z_logvar
+        x = self.decode(z)
+        return x, z_mu, z_logvar
 
-def criterion(x, x_mu, x_logvar, z_mu, z_logvar, eps=1e-6):
-    log_px = (-0.5*(math.log(2*math.pi) + x_logvar + (x-x_mu)**2/(x_logvar.exp() + eps))).sum(-1).mean()
+def criterion(x, x_pred,  z_mu, z_logvar):
+    log_px = nll_loss(x, x_pred)
     kl = (0.5*(z_logvar.exp() + z_mu**2 - z_logvar - 1)).sum(-1).mean()
-    loss = -log_px + kl
+    loss = log_px + kl
     return loss, log_px, kl
 
 
 vae = VAE(n_x, n_z)
 optimizer = optim.Adam(vae.parameters(), lr=lr)
 
+z = Variable(torch.zeros(10,100).normal_())
+
 for e in range(n_epochs):
     for data in data_loader:
         x = Variable(data)
         optimizer.zero_grad()
 
-        x_mu, x_logvar, z_mu, z_logvar = vae(x)
-        loss, log_px, kl = criterion(x, x_mu, x_logvar, z_mu, z_logvar)
+        x_pred, z_mu, z_logvar = vae(x)
+        loss, log_px, kl = criterion(x.long(), x_pred, z_mu, z_logvar)
         loss.backward()
         optimizer.step()
-        print loss.data[0]
+
+    print loss.data[0], log_px.data[0], kl.data[0]
+
+    _, x_pred  = vae.decode(z).max(2)
+    print x_pred.sum(1).data.numpy().squeeze()
+    print examples.sum(1).numpy().squeeze()
+
+    x_pred, z_mu, z_logvar = vae(Variable(examples))
+    _, x_pred = x_pred.max(2)
+    print examples.numpy()
+    print x_pred.data.numpy().squeeze()
