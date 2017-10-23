1464e58c4c8f7f99543e7264fcc14528c6f92a19
Gabriel Huang <gbxhuang@gmail.com>
2017-10-23 17:45:09 -0400

    fix type

--- visual-hyperplane/hyperplane_dataset.py
+++ visual-hyperplane/hyperplane_dataset.py
@@ -90,7 +90,7 @@ class HyperplaneImageDataset(Dataset):
         image = torch.zeros(len(x),28,28)
         for i in range(len(x)):
             image[i] = self.images[np.random.choice(self.idx[int(x[i])])][0]
-        return image, x.numpy()
+        return image, x
 
 def get_full_train_test(amount, coins, n_coins, one_hot, validation=0.8, seed=None):
     # Get main dataset
