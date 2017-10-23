1daf7b1754478ad36be8e78076ebe73bc3dec3cb
Ahmed Touati <ahmed.touati90@gmail.com>
2017-10-23 19:09:56 -0400

    implement images generator

--- visual-hyperplane/models_gan.py
+++ visual-hyperplane/models_gan.py
@@ -8,6 +8,39 @@ import torch.nn.functional as F
 # Networks
 ########################
 
+def build_cnn(sizes, batchnorm=True, deconv=False):
+    '''
+    sizes: (output channel, kernel_size, stride, padding)
+    '''
+    modules = []
+    for i, (out_ch, kernel, stride, padding) in enumerate(sizes):
+        if i == 0:
+            continue
+        in_ch = sizes[i-1][0]
+        if deconv:
+            modules.append(
+                nn.ConvTranspose2d(in_ch, out_ch, kernel, stride, padding))
+        else:
+            modules.append(
+                nn.Conv2d(in_ch, out_ch, kernel, stride, padding))
+        if batchnorm:
+            modules.append(
+                nn.BatchNorm2d(out_ch))
+        if i != len(sizes)-1:
+            modules.append(
+                nn.ReLU(True))
+    return modules
+
+
+def build_mlp(sizes, batchnorm=True):
+    modules = []
+    for (in_dim, out_dim) in sizes:
+        modules.append(nn.Linear(in_dim, out_dim))
+        if batchnorm:
+            modules.append(nn.BatchNorm1d(out_dim))
+        modules.append(nn.ReLU(True))
+    return modules
+
 
 def gumbel_softmax_sampler(logits, tau, use_cuda):
     noise = torch.rand(logits.size())
@@ -104,3 +137,56 @@ class DigitDiscriminator(nn.Module):
         out = input.view(-1, N)
         out = self.main(out)
         return out.view(-1, 1)
+
+
+class ImageGenerator(nn.Module):
+    def __init__(self, latent_global, latent_visual, nb_digits):
+        nn.Module.__init__(self)
+        self.nb_digits = nb_digits
+        self.latent_global = latent_global
+        self.latent_visual = latent_visual
+
+        self.latent_sizes = [
+            [latent_global, 128],
+            [128, 128],
+            [128, nb_digits * latent_visual]
+        ]
+        self.deconv_sizes = [
+            [latent_visual, None, None, None],
+            # 1 x 1 - latent
+            [128, 4, 1, 0],
+            # 4 x 4 - 128
+            [128, 4, 2, 1],
+            # 8 x 8 - 128
+            [64, 4, 2, 2],
+            # 14 x 14 - 64
+            [1, 4, 2, 1]
+            # 28 x 18 - 1
+        ]
+        self.get_latents = nn.Sequential(
+            *build_mlp(self.latent_sizes, batchnorm=True)
+        )
+        self.get_images = nn.Sequential(
+            *build_cnn(self.deconv_sizes, batchnorm=True)
+        )
+
+    def forward(self, input):
+        latent_vars = self.get_latents(input)
+        latent_vars = latent_vars.view(-1, self.latent_visual)
+        images = self.get_images(latent_vars)
+        images = images.view(-1, self.nb_digits, 28, 28)
+        return images
+
+    def generate(self, batch_size, use_cuda, volatile=True):
+        noise = torch.randn(batch_size, self.latent)
+        if use_cuda:
+            noise = noise.cuda()
+        noise = Variable(noise, volatile=volatile)
+        samples = self(noise)
+        return samples
+
+
+
+
+
+
