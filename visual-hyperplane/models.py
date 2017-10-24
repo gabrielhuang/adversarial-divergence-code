7dddde621c31e3013ce10b7545b5615a35a1528a
Hugo Berard <berard.hugo@gmail.com>
2017-10-23 17:55:31 -0700

    Updating models

--- visual-hyperplane/models.py
+++ visual-hyperplane/models.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
+import math
 
 class VAE(nn.Module):
     def __init__(self, n_digits, latent=50, batchnorm=True, activation=F.relu):
@@ -12,14 +13,14 @@ class VAE(nn.Module):
         if batchnorm:
             self.activation = lambda x: F.batch_norm(activation(x))
         else:
-            self.activation = self.activation
+            self.activation = activation
 
         self.enc1 = nn.Conv2d(1,8,4,2,1) #8x14x14
         self.enc2 = nn.Conv2d(8,16,4,2,2) #16x8x8
         self.enc3 = nn.Conv2d(16,32,4,2,1) #32x4x4
         self.enc4 = nn.Conv2d(32,32,4,2,0) #32x1x1
-        self.enc_mu = nn.Linear(200, self.latent)
-        self.enc_sigma = nn.Linear(200, self.latent)
+        self.enc_mu = nn.Linear(32*self.n_digits, self.latent)
+        self.enc_sigma = nn.Linear(32*self.n_digits, self.latent)
 
         self.dec1 = nn.Linear(self.latent, 200)
         self.dec2 = nn.Linear(200, 200)
@@ -55,21 +56,21 @@ class VAE(nn.Module):
         z = self.activation(self.dec2(z))
         z = self.activation(self.dec3(z))
 
-        x = self.activation(self.dec4(x.view(-1,1,28,28)))
+        x = self.activation(self.dec4(z.view(-1,32,1,1)))
         x = self.activation(self.dec5(x))
         x = self.activation(self.dec6(x))
         x = self.activation(self.dec7(x))
-        x_mu = self.dec_mu(x))
-        x_logvar = self.dec_sigma(x))
+        x_mu = F.sigmoid(self.dec_mu(x))
+        #x_logvar = self.dec_sigma(x)
 
-        return x_mu.view(-1,self.n_digits,28,28), x_logvar.view(-1,self.n_digits,28,28)
+        return x_mu.view(-1,self.n_digits,28,28)#, x_logvar.view(-1,self.n_digits,28,28)
 
 
     def forward(self, x):
         z_mu, z_logvar = self.encode(x)
         z = self.reparameterize(z_mu, z_logvar)
         x_mu, x_logvar = self.decode(z)
-        return x_mu, x_logvar, z_mu, z_logvar
+        return x_mu, z_mu, z_logvar
 
     def generate(self, batch_size=100, noise=None, volatile=False, use_cuda=True):
         if noise is None:
