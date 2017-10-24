885b252eadf9f98697ad5757fc7a6d16c95b9dff
Gabriel Huang <gbxhuang@gmail.com>
2017-10-24 15:51:55 -0400

    add slurm script

---
+++ visual-hyperplane/jobs/constrained.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python train_gan_visual.py --logdir /data/lisa/exp/huanggab/hyperplanegan --amount 25 --nb_digits 5 --latent-global $1 --model-generator constrained --model-discriminator constrained 
