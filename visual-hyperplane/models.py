4caf9f4f410336614fd49bc91ce0d0c669810098
Gabriel Huang <gbxhuang@gmail.com>
2017-10-23 20:31:53 -0400

    add unconstrained cnn model

--- visual-hyperplane/models.py
+++ visual-hyperplane/models.py
@@ -3,6 +3,30 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
+def build_cnn(sizes, batchnorm=True, deconv=False):
+    '''
+    sizes: (output channel, kernel_size, stride, padding)
+    '''
+    modules = []
+    for i, (out_ch, kernel, stride, padding) in enumerate(sizes):
+        if i == 0:
+            continue
+        in_ch = sizes[i-1][0]
+        if deconv:
+            modules.append(
+                nn.ConvTranspose2d(in_ch, out_ch, kernel, stride, padding))
+        else:
+            modules.append(
+                nn.Conv2d(in_ch, out_ch, kernel, stride, padding))
+        if batchnorm:
+            modules.append(
+                nn.BatchNorm2d(out_ch))
+        if i != len(sizes)-1:
+            modules.append(
+                nn.ReLU(True))
+    return modules
+
+
 class VAE(nn.Module):
     def __init__(self, n_digits, latent=50, batchnorm=True, activation=F.relu):
         super(VAE, self).__init__()
@@ -79,3 +103,144 @@ class VAE(nn.Module):
             noise = Variable(noise, volatile=volatile)
         x_mu, x_logvar = self.decode(noise)
         return x_mu
+
+
+class UnconstrainedVAE(nn.Module):
+    def __init__(self, latent, n_digits, batchnorm=True):
+        nn.Module.__init__(self)
+
+        self.latent = latent
+        self.n_digits = n_digits
+        self.batchnorm = batchnorm
+
+        # Build network sizes
+        self.encoder_sizes = [
+            [n_digits, None, None, None],
+            # 28 x 28 - n_digits
+            [64, 4, 2, 1],
+            # 14 x 14 - 128
+            [128, 4, 2, 2],
+            # 8 x 8 - 128
+            [128, 4, 2, 1],
+            # 4 x 4 - 128
+            [128, 4, 1, 0],
+            # 1 x 1 - 128
+        ]
+
+        self.decoder_sizes = [
+            [latent, None, None, None],
+            # 1 x 1 - latent
+            [128, 4, 1, 0],
+            # 4 x 4 - 128
+            [128, 4, 2, 1],
+            # 8 x 8 - 128
+            [64, 4, 2, 2],
+            # 14 x 14 - 128
+            [n_digits, 4, 2, 1],
+            # 28 x 28 - n_digits
+        ]
+
+        # Build encoder
+        self.encoder_modules = build_cnn(self.encoder_sizes,
+                                         batchnorm,
+                                         deconv=False)
+        self.encoder_modules == [nn.ReLU(True)]
+        self.encoder = nn.Sequential(*self.encoder_modules)
+
+        # Mean and covariance from encoder output
+        self.to_mean = nn.Conv2d(self.encoder_sizes[-1][0], self.latent, 1, 1, 0)
+        self.to_cov = nn.Conv2d(self.encoder_sizes[-1][0], self.latent, 1, 1, 0)
+        # 1 x 1 - latent
+
+        # Build decoder
+        self.decoder_modules = build_cnn(self.decoder_sizes,
+                                 batchnorm=batchnorm,
+                                 deconv=True)
+        # ReLU is better than Sigmoid because data contains mostly 0
+        # so everything will be centered but positive
+        self.decoder_modules += [nn.ReLU(True)]
+        self.decoder = nn.Sequential(*self.decoder_modules)
+
+    def encode(self, x):
+        h = self.encoder(x)
+        return self.to_mean(h), self.to_cov(h)
+
+    def reparameterize(self, mu, logvar):
+        if self.training:
+          std = logvar.mul(0.5).exp_()
+          eps = Variable(std.data.new(std.size()).normal_())
+          return eps.mul(std).add_(mu)
+        else:
+          return mu
+
+    def decode(self, z):
+        return self.decoder(z)
+
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
+
+    def generate(self, batch_size=100, noise=None, volatile=False, use_cuda=True):
+        if noise is None:
+            noise = torch.randn(batch_size, self.latent, 1, 1)
+            if use_cuda:
+                noise = noise.cuda()
+            noise = Variable(noise, volatile=volatile)
+        samples = self.decode(noise)
+        return samples
+
+
+class Generator(nn.Module):
+
+    def __init__(self, latent, resolution, batchnorm=True):
+        nn.Module.__init__(self)
+        self.latent = latent
+        self.resolution = resolution
+        self.batchnorm = batchnorm
+
+        __ , self.generator_sizes  = get_sizes(latent, resolution)
+
+        self.modules = build_cnn(self.generator_sizes,
+                              batchnorm,
+                              deconv=True)
+        self.modules += [nn.ReLU(True)]
+
+        self.main = nn.Sequential(*self.modules)
+
+    def forward(self, input):
+        out = self.main(input)
+        return out
+
+    def generate(self, batch_size=100, noise=None, volatile=False, use_cuda=True):
+        if noise is None:
+            noise = torch.randn(batch_size, self.latent, 1, 1)
+            if use_cuda:
+                noise = noise.cuda()
+            noise = Variable(noise, volatile=volatile)
+        samples = self(noise)
+        return samples
+
+
+class Discriminator(nn.Module):
+
+    def __init__(self, latent, resolution, batchnorm=False):
+        nn.Module.__init__(self)
+        self.latent = latent
+        self.resolution = resolution
+        self.batchnorm = batchnorm
+
+        self.discriminator_sizes, __ = get_sizes(latent, resolution)
+
+        self.modules = build_cnn(self.discriminator_sizes,
+                              batchnorm,
+                              deconv=False)
+        self.modules += [nn.ReLU(True)]
+        self.modules += [nn.Conv2d(self.discriminator_sizes[-1][0], 1, 1, 1, 0)]
+
+        self.main = nn.Sequential(*self.modules)
+
+    def forward(self, input):
+        out = self.main(input)
+        return out.view(-1, 1)  # scalar output
+
