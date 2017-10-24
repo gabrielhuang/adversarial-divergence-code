FIX MILA-8 GRADIENT PENALTY

mila8/train_gan_mila8.py
@@ -85,7 +85,7 @@ def get_gradient_penalty(netD, real_data, fake_data, double_sided, cuda):
     D_interpolates = netD(interpolates)
 
     ones = torch.ones(D_interpolates.size())
-    zeros = torch.zeros(batch_size)
+    zeros = Variable(torch.zeros(len(real_data)))
     if cuda:
         ones = ones.cuda()
         zeros = zeros.cuda()
@@ -97,7 +97,7 @@ def get_gradient_penalty(netD, real_data, fake_data, double_sided, cuda):
     if double_sided:
         gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
     else:
-        gradient_penalty, __ = torch.max((gradients*gradients).sum(1)-1, 0)
+        gradient_penalty = torch.max((gradients*gradients).sum(1)-1, zeros)
         gradient_penalty = gradient_penalty.mean()
 
     return gradient_penalty