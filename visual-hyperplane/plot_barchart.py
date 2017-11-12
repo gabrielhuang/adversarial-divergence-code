743562174a4312c64e9efd10fb71e5588b61aa1f
Gabriel Huang <gbxhuang@gmail.com>
2017-11-12 13:58:29 -0500

    add perfect

--- visual-hyperplane/plot_barchart.py
+++ visual-hyperplane/plot_barchart.py
@@ -15,6 +15,7 @@ gan_digits = np.load(gan_digits_filename)
 vae_sum = vae_digits.sum(axis=1)
 gan_sum = gan_digits.sum(axis=1)
 
+
 # generate baseline samples
 nb_samples = len(vae_digits)
 flat_dataset = np.array(full_dataset.hyperplane_dataset.combinations).flatten()
@@ -24,6 +25,10 @@ freq = freq / float(len(flat_dataset))
 
 base_digits = samples = np.random.choice(digits, size=(nb_samples, 5), p=freq)
 baseline_sum = samples.sum(axis=1)
+
+# generate perfect samples
+perfect_idx = np.random.randint(0, len(full_dataset), size=nb_samples)
+perfect_digits = np.asarray(full_dataset.hyperplane_dataset.combinations)[perfect_idx]
 
 x_baseline, height_baseline = np.unique(baseline_sum, return_counts=True)
 height_baseline = height_baseline/float(len(baseline_sum)) * 100
@@ -66,21 +71,27 @@ l_gan_train = []
 l_gan_test = []
 l_base_train = []
 l_base_test = []
+l_perfect_train = []
+l_perfect_test = []
 for sample_ratio in np.linspace(0, 1, 30):
     N = min(len(vae_digits), len(gan_digits))
     n_samples = max(1, min(int(sample_ratio*N), N))
 
-    recall_vae_full = get_recall(vae_digits[:n_samples], full_dataset)
+    #recall_vae_full = get_recall(vae_digits[:n_samples], full_dataset)
     recall_vae_train = get_recall(vae_digits[:n_samples], train_dataset)
     recall_vae_test = get_recall(vae_digits[:n_samples], test_dataset)
 
-    recall_gan_full = get_recall(gan_digits[:n_samples], full_dataset)
+    #recall_gan_full = get_recall(gan_digits[:n_samples], full_dataset)
     recall_gan_train = get_recall(gan_digits[:n_samples], train_dataset)
     recall_gan_test = get_recall(gan_digits[:n_samples], test_dataset)
 
-    recall_base_full = get_recall(base_digits[:n_samples], full_dataset)
+    #recall_base_full = get_recall(base_digits[:n_samples], full_dataset)
     recall_base_train = get_recall(base_digits[:n_samples], train_dataset)
     recall_base_test = get_recall(base_digits[:n_samples], test_dataset)
+
+    #recall_perfect_full = get_recall(perfect_digits[:n_samples], full_dataset)
+    recall_perfect_train = get_recall(perfect_digits[:n_samples], train_dataset)
+    recall_perfect_test = get_recall(perfect_digits[:n_samples], test_dataset)
 
     # accumulate
     l_samples.append(n_samples)
@@ -90,25 +101,31 @@ for sample_ratio in np.linspace(0, 1, 30):
     l_gan_test.append(recall_gan_test)
     l_base_train.append(recall_base_train)
     l_base_test.append(recall_base_test)
+    l_perfect_train.append(recall_perfect_train)
+    l_perfect_test.append(recall_perfect_test)
 l_samples = np.asarray(l_samples)
 
 print 'n_samples', n_samples
 
 print 'Baseline recalls:'
-print '\tfull', recall_base_full
+#print '\tfull', recall_base_full
 print '\ttrain', recall_base_train
 print '\ttest', recall_base_test
 
 print 'VAE recalls:'
-print '\tfull', recall_vae_full
+#print '\tfull', recall_vae_full
 print '\ttrain', recall_vae_train
 print '\ttest', recall_vae_test
 
 print 'GAN recalls:'
-print '\tfull', recall_gan_full
+#print '\tfull', recall_gan_full
 print '\ttrain', recall_gan_train
 print '\ttest', recall_gan_test
 
+print 'Perfect recalls:'
+#print '\tfull', recall_perfect_full
+print '\ttrain', recall_perfect_train
+print '\ttest', recall_perfect_test
 
 # Make actual figure
 plt.figure()
@@ -129,6 +146,9 @@ plt.plot(l_samples, l_vae_train, '-o', alpha=0.6, color='red', label='VAE (train
 
 plt.plot(l_samples, l_base_test, '-o', alpha=0.6, color='gray', label='Indep. baseline (test)')
 plt.plot(l_samples, l_base_train, '-o', alpha=0.6, color='gray', label='Indep. baseline (train)', linestyle='--')
+
+plt.plot(l_samples, l_perfect_test, '-o', alpha=0.6, color='blue', label='Perfect (test)')
+plt.plot(l_samples, l_perfect_train, '-o', alpha=0.6, color='blue', label='Perfect (train)', linestyle='--')
 
 plt.xlabel('samples generated')
 plt.ylabel('recall')
@@ -158,6 +178,9 @@ plt.plot(train_ratio, l_vae_train, '-', alpha=train_alpha, color='red', label='V
 
 plt.plot(test_ratio, l_base_test, '-', alpha=test_alpha, color='gray', label='Indep. baseline (test)')
 plt.plot(train_ratio, l_base_train, '-', alpha=train_alpha, color='gray', label='Indep. baseline (train)', linestyle='--')
+
+plt.plot(test_ratio, l_perfect_test, '-', alpha=test_alpha, color='blue', label='Perfect (test)')
+plt.plot(train_ratio, l_perfect_train, '-', alpha=train_alpha, color='blue', label='Perfect (train)', linestyle='--')
 
 plt.xlabel('samples generated / size(target set)')
 plt.ylabel('recall')
